c1b195531acdd9fe6852a1aae1b5e0d85854d783
David OC <davidherzlos@gmail.com>
2021-06-01 14:04:42 -0500

    Add language scaffolding

---
+++ lang/en/block_simpleportfolio.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings for the simpleportfolio block.
+ *
+ * @package    block_simpleportfolio
+ * @copyright  David OC <davidherzlos@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'simpleportfolio';
+$string['simpleportfolio'] = 'simpleportfolio';
+
+// Capabilities
+$string['simpleportfolio:addinstance'] = 'Add a new simpleportfolio block';
+$string['simpleportfolio:myaddinstance'] = 'Add a new simpleportfolio block to the Dashboard';
+
+// Edit form
+$string['header'] = 'Block specific settings';
+$string['sometext'] = 'Some text setting';
+
+// Navigation
+$string['blocksadminpage'] = 'Manage blocks';
